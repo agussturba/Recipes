91c70cc418b4c4e4d5af6b9aef1351909f4de16e
agussturba <49353314+agussturba@users.noreply.github.com>
2022-06-11 17:23:34 -0300

    -Deployment issues

--- src/main/java/com/uade/recipes/RecipesApplication.java
+++ src/main/java/com/uade/recipes/RecipesApplication.java
@@ -1,15 +1,13 @@
 package com.uade.recipes;
 
-import com.opencsv.exceptions.CsvException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.IOException;
 
 
 @SpringBootApplication
 public class RecipesApplication {
-	public static void main(String[] args) throws IOException, CsvException {
+	public static void main(String[] args)  {
 		SpringApplication.run(RecipesApplication.class, args);
 	}
 
